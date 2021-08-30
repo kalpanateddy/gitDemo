649d6095be11c4cd4475ad5ede5e45e7250575b8
kalpanateddy <jeenukalpana@gmail.com>
2021-08-30 10:43:53 +0530

    demo1

---
+++ src/test/java/org/git/Demo.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class Demo {
+	
+	
+	
+	public class FailClass {
+		int min =0,max=5;
+		public boolean retry() {
+			if(min<max) {
+				min++;
+				return true;
+			}
+			return false;
+			}
+
+		}
+
+
+
+
+}
